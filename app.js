c72fdadb87e2b17178189b56cbd9b542e2956ca9
Andrew Wong <andrew.wongsf@gmail.com>
2017-03-18 14:49:15 -0400

    updated Twitter API requests and added requested data to pug templates

--- app.js
+++ app.js
@@ -1,28 +1,106 @@
 'use strict';
 
-var T = require('./public/js/config.js');
-var express = require('express');
+var T = require('./public/js/config.js'),
+	express = require('express'),
+	moment = require('moment');
 
 var app = express();
+
+var accountList = {},
+	timelineList = {},
+	friendsList = {},
+	msgList = {};
+
+moment.updateLocale('en', {
+    relativeTime : {
+        future: "in %s",
+        past:   "%s ago",
+        s:  "s",
+        m:  "1m",
+        mm: "%dm",
+        h:  "1h",
+        hh: "%dh",
+        d:  "1d",
+        dd: "%dd",
+        M:  "1mo",
+        MM: "%dmo",
+        y:  "1y",
+        yy: "%dy"
+    }
+});
 
 app.use('/static', express.static(__dirname + '/public'))
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/templates');
 
-app.get('/', function(req, res){
-	var path = req.path;
-	res.locals.path = path;
-	res.render('index');
+T.get('account/verify_credentials', function(err, data, response) {
+
+	accountList["screenName"] = data.screen_name;
+	accountList["profileImgURL"] = data.profile_image_url;
+	accountList["profileBgImgURL"] = data.profile_background_image_url;
+	accountList["friendsCount"] = data.friends_count;
+	console.log(accountList);
+
+	T.get('statuses/home_timeline', { count: 5 }, function(err, data, response) {
+
+		timelineList = data.map(function (obj) {
+			var list = {};
+			list["text"] = obj.text;
+			list["retweets"] = obj.retweet_count;
+			list["likes"] = obj.favorite_count;
+			list["dateTweeted"] = moment(obj.created_at, "dd MMM DD HH:mm:ss ZZ YYYY", "en").fromNow(true);
+			list["name"] = obj.user["name"];
+			list["userScreenName"] = obj.user["screen_name"];
+			list["userProfileImgURL"] = obj.user["profile_image_url"];
+			return list;
+		});
+
+		console.log(timelineList);
+
+			T.get('friends/list', { count: 5 }, function(err, data, response) {
+
+				friendsList = data["users"].map(function (obj) {
+					var list = {};
+					list["profileImageURL"] = obj.profile_image_url;
+					list["name"] = obj.name;
+					list["screenName"] = obj.screen_name;
+					list["following"] = obj.following;
+					return list;
+				});
+
+				console.log(friendsList);
+
+				T.get('direct_messages', { count: 5 }, function(err, data, response) {
+
+					msgList = data.map(function (obj) {
+						var list = {};
+						list["senderProfileImageURL"] = obj.sender["profile_image_url"];
+						list["senderName"] = obj.sender["name"];
+						list["messageBody"] = obj.text;
+						list["date"] = moment(obj.created_at).format("MMM Do h:mm a");
+						return list;
+					});
+
+					console.log(msgList);
+				})
+			})
+	})
+})
+
+app.get('/', function(req, res) {
+
+	res.render('index', { 
+		accountList: accountList,
+		timelineList: timelineList,
+		friendsList: friendsList,
+		msgList: msgList
+	});
 });
 
-//
-//  search twitter for all tweets containing the word 'banana' since July 11, 2011
-//
-// T.get('search/tweets', { q: 'banana since:2011-07-11', count: 100 }, function(err, data, response) {
-//   console.log(data)
-// })
-
-app.listen(3000, '0.0.0.0', function() {
+app.listen(3000, function() {
 	console.log("The frontend server is running on port 3000!");
 });
+
+
+
